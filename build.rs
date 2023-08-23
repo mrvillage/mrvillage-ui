59798fceae27cbfdb058c67c13c0d1d9381e626d
mrvillage <josef@mrvillage.dev>
2023-08-23 00:57:26 -0400

    Update command

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 use std::process::Command;
 
 fn main() {
+    Command::new("npm").args(["install"]).output().unwrap();
     Command::new("postcss")
         .args(["-o", "./src/output.css", "./src/input.css"])
         .output()
