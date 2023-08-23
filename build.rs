ef9931166e6f2bc09e8a43e81a6a8283930e5997
mrvillage <josef@mrvillage.dev>
2023-08-23 01:07:18 -0400

    Fix directories

--- build.rs
+++ build.rs
@@ -1,9 +1,10 @@
 use std::process::Command;
 
 fn main() {
-    Command::new("npm").args(["install"]).output().unwrap();
-    Command::new("postcss")
-        .args(["-o", "./src/output.css", "./src/input.css"])
+    println!("TEST");
+    println!("{}", std::str::from_utf8(&Command::new("npm").args(["install"]).output().unwrap().stdout).unwrap());
+    println!("{}", std::str::from_utf8(&Command::new("postcss")
+        .args(["-o", "./css/output.css", "./css/input.css"])
         .output()
-        .unwrap();
+        .unwrap().stdout).unwrap());
 }
