0f655a220eac3f88c4c3cb04282e9c7fbfd1041c
mrvillage <josef@mrvillage.dev>
2023-08-31 23:46:19 -0400

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,9 +1,36 @@
 use std::process::Command;
 
 fn main() {
-    println!("{}", std::str::from_utf8(&Command::new("npm").args(["install"]).output().unwrap().stdout).unwrap());
-    println!("{}", std::str::from_utf8(&Command::new("postcss")
-        .args(["-o", "./css/output.css", "./css/input.css"])
-        .output()
-        .unwrap().stdout).unwrap());
+    for entry in std::fs::read_dir("./src").unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if path.is_file() {
+            let path = path.to_str().unwrap();
+            if path.ends_with(".rs") {
+                println!("cargo:rerun-if-changed={}", path);
+            }
+        }
+    }
+    println!(
+        "{}",
+        std::str::from_utf8(
+            &Command::new("npm")
+                .args(["install"])
+                .output()
+                .unwrap()
+                .stdout
+        )
+        .unwrap()
+    );
+    println!(
+        "{}",
+        std::str::from_utf8(
+            &Command::new("postcss")
+                .args(["-o", "./css/output.css", "./css/input.css"])
+                .output()
+                .unwrap()
+                .stdout
+        )
+        .unwrap()
+    );
 }
