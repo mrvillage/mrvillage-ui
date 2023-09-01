a7cb8e97445571ab4daf7d9bbba02fd250a4a76e
mrvillage <josef@mrvillage.dev>
2023-09-01 00:08:24 -0400

    Add default instead

--- src/components/number_input.rs
+++ src/components/number_input.rs
@@ -3,15 +3,15 @@ use leptos::{component, view, IntoAttribute, IntoProperty, IntoView, MaybeSignal
 #[component]
 pub fn NumberInput<T>(
     cx: Scope,
-    #[prop(into, optional)] value: MaybeSignal<T>,
+    #[prop(into, default = MaybeSignal::Static((0 as usize).into()))] value: MaybeSignal<T>,
     #[prop(into, optional)] class: MaybeSignal<String>,
 ) -> impl IntoView
 where
-    T: IntoAttribute + Default + Clone + 'static,
+    T: IntoAttribute + Default + Clone + From<usize> + 'static,
     (leptos::Scope, MaybeSignal<T>): IntoProperty,
 {
     let class = move || format!("mu-input {}", class());
     view! { cx,
-        <input type="text" class=class prop:value=value />
+        <input type="text" inputmode="numeric" class=class prop:value=value />
     }
 }
