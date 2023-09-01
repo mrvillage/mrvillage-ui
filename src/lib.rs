3e3076b96f7737339bf94b4a7d781dd86bffda82
mrvillage <josef@mrvillage.dev>
2023-08-31 23:45:12 -0400

    Update dir

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,4 @@ mod components;
 
 pub use components::*;
 
-pub const CSS: &'static str = include_str!("../css/output.css");
+pub const CSS: &'static str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/css/output.css"));
