92f17e96bf6dad09df121484857099ad78d5daa3
mrvillage <josef@mrvillage.dev>
2023-09-15 13:55:59 -0400

    Put button styles into classes

--- src/components/button.rs
+++ src/components/button.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::fmt::{Debug, Display};
 
 use leptos::{component, view, Children, IntoAttribute, IntoView, MaybeSignal};
 use serde::{Deserialize, Serialize};
@@ -6,11 +6,18 @@ use serde::{Deserialize, Serialize};
 #[component]
 pub fn Button(
     children: Children,
-    #[prop(into)] color: MaybeSignal<ButtonColor>,
+    #[prop(into, optional)] color: MaybeSignal<Option<ButtonColor>>,
     #[prop(into, optional)] size: MaybeSignal<ButtonSize>,
     #[prop(into, optional)] class: MaybeSignal<String>,
 ) -> impl IntoView {
-    let class = move || format!("mu-button {} {} {}", color(), size(), class(),);
+    let class = move || {
+        format!(
+            "mu-button {} {} {}",
+            color().map(|s| s.to_string()).unwrap_or_else(String::new),
+            size(),
+            class(),
+        )
+    };
     view! {
         <button class=class>
             {children()}
@@ -40,7 +47,6 @@ pub enum ButtonColor {
     Fuchsia,
     Pink,
     Rose,
-    None,
     Transparent,
 }
 
@@ -49,128 +55,27 @@ impl Display for ButtonColor {
         use ButtonColor::*;
 
         match self {
-            Black => {
-                write!(
-                    f,
-                    "tw-bg-black hover:tw-bg-stone-700 active:tw-bg-stone-600"
-                )
-            },
-            White => {
-                write!(
-                    f,
-                    "tw-bg-white hover:tw-bg-stone-100 active:tw-bg-stone-200"
-                )
-            },
-            Stone => {
-                write!(
-                    f,
-                    "tw-bg-stone-600 hover:tw-bg-stone-700 active:tw-bg-stone-800"
-                )
-            },
-            Red => write!(f, "tw-bg-red-600 hover:tw-bg-red-700 active:tw-bg-red-800"),
-            Orange => {
-                write!(
-                    f,
-                    "tw-bg-orange-600 hover:tw-bg-orange-700 active:tw-bg-orange-800"
-                )
-            },
-            Amber => {
-                write!(
-                    f,
-                    "tw-bg-amber-600 hover:tw-bg-amber-700 active:tw-bg-amber-800"
-                )
-            },
-            Yellow => {
-                write!(
-                    f,
-                    "tw-bg-yellow-600 hover:tw-bg-yellow-700 active:tw-bg-yellow-800"
-                )
-            },
-            Lime => {
-                write!(
-                    f,
-                    "tw-bg-lime-600 hover:tw-bg-lime-700 active:tw-bg-lime-800"
-                )
-            },
-            Green => {
-                write!(
-                    f,
-                    "tw-bg-green-600 hover:tw-bg-green-700 active:tw-bg-green-800"
-                )
-            },
-            Emerald => {
-                write!(
-                    f,
-                    "tw-bg-emerald-600 hover:tw-bg-emerald-700 active:tw-bg-emerald-800"
-                )
-            },
-            Teal => {
-                write!(
-                    f,
-                    "tw-bg-teal-600 hover:tw-bg-teal-700 active:tw-bg-teal-800"
-                )
-            },
-            Cyan => {
-                write!(
-                    f,
-                    "tw-bg-cyan-600 hover:tw-bg-cyan-700 active:tw-bg-cyan-800"
-                )
-            },
-            Sky => write!(f, "tw-bg-sky-600 hover:tw-bg-sky-700 active:tw-bg-sky-800"),
-            Blue => {
-                write!(
-                    f,
-                    "tw-bg-blue-600 hover:tw-bg-blue-700 active:tw-bg-blue-800"
-                )
-            },
-            Indigo => {
-                write!(
-                    f,
-                    "tw-bg-indigo-600 hover:tw-bg-indigo-700 active:tw-bg-indigo-800"
-                )
-            },
-            Violet => {
-                write!(
-                    f,
-                    "tw-bg-violet-600 hover:tw-bg-violet-700 active:tw-bg-violet-800"
-                )
-            },
-            Purple => {
-                write!(
-                    f,
-                    "tw-bg-purple-600 hover:tw-bg-purple-700 active:tw-bg-purple-800"
-                )
-            },
-            Fuchsia => {
-                write!(
-                    f,
-                    "tw-bg-fuchsia-600 hover:tw-bg-fuchsia-700 active:tw-bg-fuchsia-800"
-                )
-            },
-            Pink => {
-                write!(
-                    f,
-                    "tw-bg-pink-600 hover:tw-bg-pink-700 active:tw-bg-pink-800"
-                )
-            },
-            Rose => {
-                write!(
-                    f,
-                    "tw-bg-rose-600 hover:tw-bg-rose-700 active:tw-bg-rose-800"
-                )
-            },
-            None => write!(f, ""),
-            Transparent => {
-                write!(
-                    f,
-                    "tw-bg-black tw-bg-opacity-0 hover:tw-bg-opacity-10 active:bg-opacity-20"
-                )
-            },
-        }?;
-        match self {
-            White => write!(f, " tw-text-black"),
-            None => write!(f, ""),
-            _ => write!(f, " tw-text-white"),
+            Black => write!(f, "mu-button-black"),
+            White => write!(f, "mu-button-white"),
+            Stone => write!(f, "mu-button-stone"),
+            Red => write!(f, "mu-button-red"),
+            Orange => write!(f, "mu-button-orange"),
+            Amber => write!(f, "mu-button-amber"),
+            Yellow => write!(f, "mu-button-yellow"),
+            Lime => write!(f, "mu-button-lime"),
+            Green => write!(f, "mu-button-green"),
+            Emerald => write!(f, "mu-button-emerald"),
+            Teal => write!(f, "mu-button-teal"),
+            Cyan => write!(f, "mu-button-cyan"),
+            Sky => write!(f, "mu-button-sky"),
+            Blue => write!(f, "mu-button-blue"),
+            Indigo => write!(f, "mu-button-indigo"),
+            Violet => write!(f, "mu-button-violet"),
+            Purple => write!(f, "mu-button-purple"),
+            Fuchsia => write!(f, "mu-button-fuchsia"),
+            Pink => write!(f, "mu-button-pink"),
+            Rose => write!(f, "mu-button-rose"),
+            Transparent => write!(f, "mu-button-transparent"),
         }
     }
 }
